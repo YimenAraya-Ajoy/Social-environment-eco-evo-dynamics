e13cedf2d8a0e096e89ef4dbc19cfdc3ead5bff4
YimenAraya-Ajoy <yimencr@gmail.com>
2025-08-25 15:24:08 +0200

    Delete Figures/Figure2.R

--- Figures/Figure2.R
+++
@@ -1,7 +0,0 @@
-setwd("/home/yi/Dropbox/SocialFitnessEffects")
-m<-matrix(1:6,2,3, byrow = "TRUE")
-pdf("Figures/Fig_2.pdf", height= 7, width=7)
-layout(m, heights = c(1,1), widths = c(1,1,0.3))
-source("Code/Figures/DensityRegulationFigure.R")
-source("Code/Figures/DensityDependentSelectionFigure.R")
-dev.off()
