f3bf81667cca1dbe140bebae2031d22c333b1579
YimenAraya-Ajoy <yimencr@gmail.com>
2025-08-25 15:19:26 +0200

    Add files via upload

---
+++ Simulations/load_results.r
@@ -0,0 +1,9 @@
+
+setwd("/home/yi/Dropbox/SocialFitnessEffects")
+
+load("Results/AdditiveFD.RData")
+load("Results/RelativeFD1.RData")
+load("Results/RelativeFD2.RData")
+load("Results/MultiplicativeFD.RData")
+load("Results/FDDenistyRegulation.RData")
+load("Results/DDFDSelection.RData")
