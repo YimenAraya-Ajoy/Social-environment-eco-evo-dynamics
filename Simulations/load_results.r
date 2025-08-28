bc4c616ed8c78647e90dd9ba86c4b52ffe21b3fb
YimenAraya-Ajoy <yimencr@gmail.com>
2025-08-28 13:13:16 +0200

    Delete Simulations/load_results.r

--- Simulations/load_results.r
+++
@@ -1,9 +0,0 @@
-
-setwd("/home/yi/Dropbox/SocialFitnessEffects")
-
-load("Results/AdditiveFD.RData")
-load("Results/RelativeFD1.RData")
-load("Results/RelativeFD2.RData")
-load("Results/MultiplicativeFD.RData")
-load("Results/FDDenistyRegulation.RData")
-load("Results/DDFDSelection.RData")
